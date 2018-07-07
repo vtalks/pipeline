1844f41e613e094a5e14425962268135fe38bc40
Raül Pérez <repejota@gmail.com>
2018-07-08 01:35:07 +0200

    Added playlist fetch raw youtube data

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# luigi --module tasks.channels vtalks.channels.FetchRawYoutubeData --youtube-url https://www.youtube.com/channel/UCSRhwaM00ay0fasnsw6EXKA
+luigi --module tasks.playlists vtalks.playlists.FetchRawYoutubeData --youtube-url https://www.youtube.com/playlist?list=PLMW8Xq7bXrG7XGG29sXso2hYYNW_14s_A
+
