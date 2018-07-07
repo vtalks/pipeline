21f22877f3d5811c3881d3f62eaa309cb1f334d4
Raül Pérez <repejota@gmail.com>
2018-07-08 01:04:38 +0200

    check if task is complete

--- tasks/channels/fetch_raw_youtube_data.py
+++ tasks/channels/fetch_raw_youtube_data.py
@@ -1,5 +1,7 @@
 import os
 import json
+from datetime import datetime
+
 import luigi
 
 from youtube_data_api3 import channel
@@ -7,37 +9,46 @@ from youtube_data_api3 import channel
 
 class FetchRawYoutubeData(luigi.Task):
     youtube_url = luigi.Parameter()
-    channel_code = None
     task_namespace = 'vtalks.channels'
 
     def requires(self):
         return []
 
     def output(self):
-        channel_code = channel.get_channel_code(self.youtube_url)
+        output_path = self._get_output_path()
+        return luigi.LocalTarget(output_path)
 
-        output_path = "/pipeline/data/youtube/channels/{:s}.json"
-
-        # Check output_path modification date
-        if self._is_outdated(output_path):
+    def complete(self):
+        is_complete = super(FetchRawYoutubeData, self).complete()
+        if not is_complete:
             return False
 
-        return luigi.LocalTarget(output_path.format(channel_code))
+        # Check output_path modification date
+        if self._is_outdated():
+            is_complete = False
+
+        return is_complete
 
     def run(self):
-        print("Executing task: {task}".format(task=self.__class__.__name__))
-
         youtube_api_token = os.getenv("YOUTUBE_API_KEY")
         channel_code = channel.get_channel_code(self.youtube_url)
-
+        
         youtube_json_data = channel.fetch_channel_data(youtube_api_token, channel_code)
         with self.output().open('w') as f:
             f.write(json.dumps(youtube_json_data))
 
-    def _is_outdated(self, output_path):
-        timestamp = os.path.getmtime(output_path)
-        print("Output modified {:d}".format(timestamp))
-        return False
+    def _get_output_path(self):
+        channel_code = channel.get_channel_code(self.youtube_url)
+        return "/pipeline/data/youtube/channels/{:s}.json".format(channel_code)
+
+    def _is_outdated(self):
+        """ Check if output modification date is older than a day
+        """
+        timestamp = os.path.getmtime(self.output().path)
+        updated = datetime.fromtimestamp(timestamp)
+        d = datetime.now() - updated
+        is_outdated = d.total_seconds() > 86400    # one day
+        return is_outdated
 
 
 if __name__ == "__main__":
